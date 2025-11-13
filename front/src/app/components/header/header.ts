1bcb7ddb1179e79dddb95e93a58c06f0e1a59454
moudjane <mathis.oudjane@gmail.com>
2025-11-13 22:35:13 +0100

    add global nav header

--- front/src/app/components/header/header.ts
+++ front/src/app/components/header/header.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth-api.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './header.html',
-  styleUrls: ['./header.scss']
+  styleUrl: './header.scss',
 })
 export class Header {
+  protected readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
+  handleLogout() {
+    this.authService.logout();
+    localStorage.removeItem('likedPostId');
+    this.router.navigateByUrl('/login');
+  }
 }
