bebd79e76c7d74a2edc78cd6cc90c5dfe2466b1f
jonathan hayot <jonathan.hayot@efrei.net>
2025-11-15 15:32:47 +0100

    add auth token in app config

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHeaders, provideHttpClient } from '@angular/common/http';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client';
+import { InMemoryCache } from '@apollo/client/core';
+import { ApolloLink } from '@apollo/client/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,10 +15,26 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideHttpClient(), provideApollo(() => {
       const httpLink = inject(HttpLink);
 
+      const http = httpLink.create({
+        uri: 'http://localhost:4000/graphql',
+      });
+
+      const authMiddleware = new ApolloLink((operation, forward) => {
+        const token = typeof window !== 'undefined'
+          ? localStorage.getItem('auth_token')
+          : null;
+
+        if (token) {
+          operation.setContext(({ headers = new HttpHeaders() }: { headers?: HttpHeaders }) => ({
+            headers: headers.set('Authorization', `Bearer ${token}`),
+          }));
+        }
+
+        return forward(operation);
+      });
+
       return {
-        link: httpLink.create({
-          uri: 'http://localhost:4000/graphql',
-        }),
+        link: authMiddleware.concat(http),
         cache: new InMemoryCache(),
       };
     })
